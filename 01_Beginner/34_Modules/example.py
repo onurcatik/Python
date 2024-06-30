69837da07ef4b91bc32b7dd6ebcf49d1d7c5c1ae
onurcatik <catikonur@gmail.com>
2024-06-30 12:51:22 +0300

    modules fixed

--- 01_Beginner/34_Modules/example.py
+++ 01_Beginner/34_Modules/example.py
@@ -1,40 +1,13 @@
-import math
-import math as m
-
-print(math.pi)
-print(math.sqrt(16))
-
-print(m.pi)
-print(m.sqrt(4))
-
-
-
-# ------------------
-
-import math
+pi = 3.141592653589793
 
 def square(x):
-    return x * x
+    return x ** 2
 
 def cube(x):
-    return x * x * x
+    return x ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
 
 def area(radius):
-    return math.pi * radius * radius
-
-
-result = math.pi
-print(result)  
-
-
-result = square(3)
-print(result) 
-
-result = cube(3)
-print(result)
-
-result = area(2)
-print(result) 
-
-
-
+    return pi * (radius ** 2)
---
+++ 01_Beginner/34_Modules/main.py
@@ -0,0 +1,16 @@
+import example
+
+result = example.pi
+print(result)  # Output: 3.141592653589793
+
+result = example.square(3)
+print(result)  # Output: 9
+
+result = example.cube(3)
+print(result)  # Output: 27
+
+result = example.circumference(3)
+print(result)  # Output: 18.84955592153876
+
+result = example.area(3)
+print(result)  # Output: 28.274333882308138
